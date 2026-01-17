092dd5ed4059f9831c886f6b0d7bfc95ff78070e
mervynchua01 <mervynchua01@gmail.com>
2026-01-17 14:30:18 +0800

    final commit

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ const winningCombos = [
   [1, 4, 7],
   [2, 5, 8],
   [0, 4, 8],
-  [2, 4, 6]
+  [2, 4, 6],
 ];
 
 /*---------------------------- Variables (state) ----------------------------*/
@@ -22,6 +22,8 @@ let tie;
 
 const squareEls = document.querySelectorAll(".sqr");
 const messageEl = document.getElementById("message");
+const boardEl = document.querySelector(".board");
+const resetButton = document.getElementById("reset");
 
 /*-------------------------------- Functions --------------------------------*/
 
@@ -31,7 +33,11 @@ function init() {
   winner = false;
   tie = false;
   render();
-  console.log("Game initialised");
+}
+
+function render() {
+  updateBoard();
+  updateMessage();
 }
 
 function updateBoard() {
@@ -51,11 +57,76 @@ function updateMessage() {
   }
 }
 
-function render() {
-  updateBoard();
-  updateMessaeg();
+function handleClick(event) {
+  const clickedElement = event.target;
+
+  if (!clickedElement.classList.contains("sqr")) {
+    return;
+  }
+
+  const squareIndex = clickedElement.id;
+
+  if (board[squareIndex] !== "") {
+    return;
+  }
+
+  if (winner) {
+    return;
+  }
+
+  placePiece(squareIndex);
+  checkForWinner();
+  checkForTie();
+  switchPlayerTurn();
+  render();
+}
+
+function placePiece(index) {
+  board[index] = turn;
+}
+
+function checkForWinner() {
+  winningCombos.forEach((combo) => {
+    const pos1 = combo[0];
+    const pos2 = combo[1];
+    const pos3 = combo[2];
+
+    if (
+      board[pos1] !== "" &&
+      board[pos1] === board[pos2] &&
+      board[pos1] === board[pos3]
+    ) {
+      winner = true;
+    }
+  });
+}
+
+function checkForTie() {
+  if (winner) {
+    return;
+  }
+
+  if (board.includes("")) {
+    tie = false;
+  } else {
+    tie = true;
+  }
+}
+
+function switchPlayerTurn() {
+  if (winner) {
+    return;
+  }
+
+  if (turn === "X") {
+    turn = "O";
+  } else {
+    turn = "X";
+  }
 }
 
 /*----------------------------- Event Listeners -----------------------------*/
 
+boardEl.addEventListener("click", handleClick);
+resetButton.addEventListener("click", init);
 init();
