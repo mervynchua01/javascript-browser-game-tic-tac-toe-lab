e38dce79183c2c02514d7910e1746ee17b82e234
mervynchua01 <mervynchua01@gmail.com>
2026-01-17 13:40:01 +0800

    step 3-5

--- js/app.js
+++ js/app.js
@@ -1,13 +1,22 @@
 /*-------------------------------- Constants --------------------------------*/
-init();
 
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-let board
-let turn
-let winner
-let tie
+let board;
+let turn;
+let winner;
+let tie;
 
 /*------------------------ Cached Element References ------------------------*/
 
@@ -16,14 +25,37 @@ const messageEl = document.getElementById("message");
 
 /*-------------------------------- Functions --------------------------------*/
 
-function init(params) {
-  board = ["","","","","","","","",""]
-  turn = 'X'
-  winner = false
-  tie = false
-  render ()
-  console.log('Game initialised')
+function init() {
+  board = ["", "", "", "", "", "", "", "", ""];
+  turn = "X";
+  winner = false;
+  tie = false;
+  render();
+  console.log("Game initialised");
 }
 
+function updateBoard() {
+  board.forEach((cell, index) => {
+    const square = squareEls[index];
+    square.textContent = cell;
+  });
+}
+
+function updateMessage() {
+  if (winner === false && tie === false) {
+    messageEl.textContent = `Player ${turn}'s turn`;
+  } else if (winner === false && tie === true) {
+    messageEl.textContent = `It is a tie!`;
+  } else {
+    messageEl.textContent = `Player ${turn} wins!`;
+  }
+}
+
+function render() {
+  updateBoard();
+  updateMessaeg();
+}
 
 /*----------------------------- Event Listeners -----------------------------*/
+
+init();
